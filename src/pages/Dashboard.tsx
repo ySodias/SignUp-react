453f4db657fcdc0ddf46c71e1bdc0a5ce53fc7f5
ySodias <63486008+ySodias@users.noreply.github.com>
2023-05-09 23:19:03 -0300

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -27,7 +27,7 @@ const Dashboard: React.FC<IDashboardProps > = (props) => {
         <>
         <iframe title="dashboard-signup" 
         style={rowStyle}
-        src="https://app.powerbi.com/reportEmbed?reportId=f3b53f52-b83a-4e60-9082-f1e6f193906f&autoAuth=true&ctid=cf72e2bd-7a2b-4783-bdeb-39d57b07f76f" 
+        src="https://app.powerbi.com/reportEmbed?reportId=bbda0a9f-aeb5-4c46-a55e-3b784f0fcb5d&autoAuth=true&ctid=cf72e2bd-7a2b-4783-bdeb-39d57b07f76f" 
         frameborder="0" allowFullScreen="true"></iframe> 
     </>
 )
